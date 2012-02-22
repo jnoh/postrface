3e874581a6b50c63ef8a0b872347f3adc0f8bc94
Jeffrey Noh <jnoh12388@gmail.com>
2012-02-22 04:55:21 -0500

    some adjustments

--- app/controllers/auth/sessions_controller.rb
+++ app/controllers/auth/sessions_controller.rb
@@ -13,6 +13,7 @@ class Auth::SessionsController < ApplicationController
         format.html { redirect_back_or_to(@user, :notice => 'Login successful.') }
         format.xml { render :xml => @user, :status => :created, :location => @user }
       else
+        @user = User.new
         format.html { flash.now[:alert] = "Log in failed."; render :action => "new" }
         format.xml { render :xml => @user.errors, :status => :unprocessable_entity }
       end
